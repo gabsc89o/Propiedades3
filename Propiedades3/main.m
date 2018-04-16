745e9d76178e0be8876e9317f91fb4f0c2c1e4c4
gabsc89o <user138066@eu249.local>
2018-04-16 16:52:42 +0100

    Initial Commit

---
+++ Propiedades3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Propiedades3
+//
+//  Created by user138066 on 4/16/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
